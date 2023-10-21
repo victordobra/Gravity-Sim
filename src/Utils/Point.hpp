100f0e8706ee91f5a495288fdb232583c0d6de37
MMG <73362859+victordobra@users.noreply.github.com>
2023-10-21 18:03:26 +0300

    Added Vector2 and point structs and an event class

---
+++ src/Utils/Point.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace gsim {
+    /// @brief A struct representing a 2D vector.
+    struct Vector2 {
+        /// @brief The X dimension of the vector.
+        double x;
+        /// @brief The Y dimension of the vector.
+        double y;
+    };
+
+    /// @brief A struct representing a simulated point.
+    struct Point {
+        /// @brief The position of the point.
+        Vector2 pos;
+        /// @brief The velocity of the point.
+        Vector2 vel;
+        /// @brief The mass of the point.
+        double mass;
+    };
+}
