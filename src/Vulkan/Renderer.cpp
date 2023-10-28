f26096c42ada7685c6e4fa25df4b5429757b7c8f
MMG <73362859+victordobra@users.noreply.github.com>
2023-10-28 20:29:52 +0300

    Converted renderer indentation to tabs

--- src/Vulkan/Renderer.cpp
+++ src/Vulkan/Renderer.cpp
@@ -7,28 +7,28 @@
 #include "Core/SwapChain.hpp"
 
 namespace gsim {
-    // Constants
+	// Constants
 #ifdef GSIM_BUILD_MODE_DEBUG
-    const bool DEBUG_ENABLED = true;
+	const bool DEBUG_ENABLED = true;
 #else
-    const bool DEBUG_ENABLED = false;
+	const bool DEBUG_ENABLED = false;
 #endif
 
-    // Public functions
-    void CreateVulkanRenderer() {
-        // Create the core components
-        CreateVulkanInstance(DEBUG_ENABLED);
-        CreateVulkanSurface();
-        CreateVulkanDevice();
-        CreateVulkanCommandPools();
-        CreateVulkanSwapChain();
-    }
-    void DestroyVulkanRenderer() {
-        // Destroy the core components
-        DestroyVulkanSwapChain();
-        DestroyVulkanCommandPools();
-        DestroyVulkanDevice();
-        DestroyVulkanSurface();
-        DestroyVulkanInstance();
-    }
+	// Public functions
+	void CreateVulkanRenderer() {
+		// Create the core components
+		CreateVulkanInstance(DEBUG_ENABLED);
+		CreateVulkanSurface();
+		CreateVulkanDevice();
+		CreateVulkanCommandPools();
+		CreateVulkanSwapChain();
+	}
+	void DestroyVulkanRenderer() {
+		// Destroy the core components
+		DestroyVulkanSwapChain();
+		DestroyVulkanCommandPools();
+		DestroyVulkanDevice();
+		DestroyVulkanSurface();
+		DestroyVulkanInstance();
+	}
 }
